408888d267e6c6591b82a65e539ee5e506796536
cc <cc@154.com>
2024-04-19 01:08:07 +0800

    ✨ feat(指令): 增加跳转到特定行的指令
    增加了scrollToLine指令，该指令会打开一个输入栏，检测是否是数字，然后跳转到打开的文件中对应的行数。

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+function scrollToLine(line: number) {
+	const editor = vscode.window.activeTextEditor;
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -19,9 +23,32 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.createTerminal('Fellowed Terminal');
 		vscode.window.showInformationMessage('Hello World from fellowed!');
 	});
-
+	let scrollToLine = vscode.commands.registerCommand('fellowed.scrollToLine', async () => {
+		const userInput = await vscode.window.showInputBox({
+			prompt: "请输入要跳转到的行数",
+			placeHolder: "在这里输入行数",
+		});
+		const editor = vscode.window.activeTextEditor;
+		if (!userInput) {
+			vscode.window.showErrorMessage("请输入行数！");
+		}
+		if (!editor) {
+			vscode.window.showErrorMessage("请打开一个文件！");
+		}
+		try {
+			if (userInput != undefined && editor != undefined) {
+				const line = parseInt(userInput);
+				const range = editor.document.lineAt(line).range;
+				editor.selection = new vscode.Selection(range.start, range.end);
+				editor.revealRange(range);
+			}
+		} catch (err) {
+			vscode.window.showErrorMessage("请输入一个有效的行数！");
+		}
+	});
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(scrollToLine);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
