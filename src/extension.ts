c8bad71dc74de490c2f3f60d1e6801fe5dbf8cf4
SiyaOliver <122611689+SiyaOliver@users.noreply.github.com>
2024-04-18 19:17:47 +0800

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -10,17 +10,54 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "fellowed" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fellowed.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.createTerminal('Fellowed Terminal');
-		vscode.window.showInformationMessage('Hello World from fellowed!');
-	});
+    // Create a decoration type for the cursor
+    const cursorDecoration = vscode.window.createTextEditorDecorationType({
+        backgroundColor: 'rgba(255, 255, 0, 0.5)', // Semi-transparent yellow background
+        dark: {
+            backgroundColor: 'rgba(255, 255, 0, 0.5)',
+        },
+        light: {
+            backgroundColor: 'rgba(255, 255, 0, 0.5)',
+        },
+        isWholeLine: true // Decorate the whole line where the cursor is
+    });
 
-	context.subscriptions.push(disposable);
+    // Function to update the cursor decoration
+    function updateCursorDecoration(editor) {
+        if (!editor) {
+            return;
+        }
+
+        const cursorPosition = editor.selection.active;
+        const range = new vscode.Range(cursorPosition.line, 0, cursorPosition.line, 0);
+
+        editor.setDecorations(cursorDecoration, [range]);
+    }
+
+    // Subscribe to selection change events to update the cursor decoration
+    vscode.window.onDidChangeTextEditorSelection(event => {
+        updateCursorDecoration(event.textEditor);
+    }, null, context.subscriptions);
+
+    // Subscribe to active editor change events to update the cursor decoration
+    vscode.window.onDidChangeActiveTextEditor(editor => {
+        updateCursorDecoration(editor);
+    }, null, context.subscriptions);
+
+    // Initially set the decoration for the active editor
+    updateCursorDecoration(vscode.window.activeTextEditor);
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('fellowed.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.createTerminal('Fellowed Terminal');
+        vscode.window.showInformationMessage('Hello World from fellowed!');
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
