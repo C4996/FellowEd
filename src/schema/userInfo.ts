b2c5a40ede74a58bbfc45ea6823c0556958df723
Grub <grub_49@163.com>
2024-04-27 22:17:19 +0800

    feat(Schema): 修改 userInfo 内容，增加 chat 和 message 两个表

--- src/schema/userInfo.ts
+++ src/schema/userInfo.ts
@@ -1,9 +1,13 @@
 import { z } from "zod";
 
 export const userInfo = z.object({
-  machineId: z.string().uuid(),
-  name: z.string(),
-  email: z.string().email().optional(),
+  machineId: z.string().uuid(), // 从 VSCode 获取的机器码
+  userId: z.string().uuid(),
+  name: z.string(), // 用户名称
+  email: z.string().email().optional(), // 用户邮箱
+  lastLoginTime: z.string().datetime(), // 上次登录时间
+  privilege: z.enum(['maintainer', 'developer', 'visitor']).default('developer'), // 特权等级
+  
 });
 
 export type UserInfo = z.infer<typeof userInfo>;
