cdbfd82134f67bde66844e48ea9557967f9ce064
cc <cc@154.com>
2024-04-27 22:54:50 +0800

    ✨ feat: 增加获取和增加评注评论的API路由

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
  import { UserInfo } from '../schema/userInfo';
 import { publicProcedure, router } from './trpc';
 import { fileQuery } from "../schema/fileInfo";
-
+import { selectedContent,UserComment } from '../schema/userComment';
 export const appRouter = router({
   getAllUsers: publicProcedure.query(async () => {
     return [
@@ -15,6 +15,38 @@ export const appRouter = router({
       filePath,
       fileSize: 100,
     };
+  }),
+  getFileComments: publicProcedure.input(fileQuery).query(async (opts) => {
+      const {filePath} = opts.input;
+      return [
+          {
+              userId: "1",
+              comment: "hello",
+              fileId: "1",
+              selected: {
+                  fileId: "1",
+                  offset: 0,
+                  length: 10,
+                  userId: "1"
+              },
+              createdAt: "2021-03-01T00:00:00.000Z"
+          }
+      ] as UserComment[];
+  }),
+  addComment: publicProcedure.input(selectedContent).mutation(async (opts) => {
+      const {fileId,offset,length,userId} = opts.input;
+      return {
+          userId,
+          comment: "hello",
+          fileId,
+          selected: {
+              fileId,
+              offset,
+              length,
+              userId
+          },
+          createdAt: "2021-03-01T00:00:00.000Z"
+      } as UserComment;
   }),
 });
 
