f69f81245c8634f7fad4b3bacf06ad31d720c9da
grubwithu <guoqiming12345@126.com>
2024-05-02 11:05:38 +0800

    feat: add user list using tree view

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { getAllUsers, helloWorld, joinSession, jumpToLine, showFileInfo, startSession } from "./commands";
+import { userListActivate } from "./client/treeview";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "fellowed" is now active!');
-
+  userListActivate(context);
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
